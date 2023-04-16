e1b9c4e268a7eb71af321de8ae747ac0ea35d6fc
Lester Ong <lesterong776@gmail.com>
2023-04-16 23:58:05 +0800

    Add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,17 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
+  daisyui: {
+    themes: ['winter', 'halloween'],
+    darkTheme: 'halloween',
+  },
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 }
