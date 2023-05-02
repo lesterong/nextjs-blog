37b7eac972d3097099d35525c5d69cdeacda023a
Lester Ong <lesterong776@gmail.com>
2023-05-02 23:26:38 +0800

    Add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer
+      className={`container mt-2 flex max-w-4xl items-center justify-between px-6 py-4 text-xs`}
+    >
+      <div>
+        <Link
+          href="https://github.com/lesterong/nextjs-blog"
+          target="_blank"
+          className="link-fill text-primary"
+        >
+          nextjs-blog
+        </Link>
+        &nbsp;by Lester Ong.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <main className={inter.className}>
-      <Navbar />
-      <Component {...pageProps} />
+      <div className="min-h-[calc(100vh-60px)]">
+        <Navbar />
+        <Component {...pageProps} />
+      </div>
+      <Footer />
     </main>
   );
 };
